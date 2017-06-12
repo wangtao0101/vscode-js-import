c7927955ca566a1bdac0dac3081c1f9872f4bd50
wangtao0101 <yuecjn@gmail.com>
2017-06-12 13:05:21 +0800

    add importstatement test

---
+++ test/importStatement.test.ts
@@ -0,0 +1,15 @@
+import * as assert from 'assert';
+import ImportStatement, { ImportOption } from '../src/importStatement';
+import parseImport from 'parse-import-es6';
+
+suite("Interpreter Tests", () => {
+
+    test("export default class uuu;", () => {
+        // const my = new ImportStatement();
+        // const text = `
+        //     import { a as b } from 'c';
+        // `
+
+        // assert.deepEqual([{default: true, name: 'uuu'}], my.run(text, false, '', ''))
+    });
+});
